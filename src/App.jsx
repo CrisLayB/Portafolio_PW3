44c4ed8d3b3b74a1672a97ecab6963a4d9a0f5ce
CrisLBNumber2 <54615310+CRLB-sketch@users.noreply.github.com>
2022-06-02 20:22:33 -0600

    Empezando Portafolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './icons/logo.svg'
+import './css/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Cristian Fernando Laynez Bachez</h1>
+      Insetar imagen mia jeje
+    </div>
+  )
+}
+
+export default App
