1bf4386d0cee4edd4ed1d23b492e9108f99585e0
CrisLBNumber2 <54615310+CRLB-sketch@users.noreply.github.com>
2022-06-03 20:21:35 -0600

    Estructurando portafolio.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,59 @@ import { useState } from 'react'
 import logo from './icons/logo.svg'
 import './css/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './css/Styles.css'
 
+function App() {
   return (
     <div className="App">
+      <h1>Hola</h1>
+      <h1>Mi nombre es...</h1>
       <h1>Cristian Fernando Laynez Bachez</h1>
       Insetar imagen mia jeje
+      <h2>Ingeniero en Ciencias de la Computacion</h2>
+
+      <p></p>
+      <h1>[*Insertar un poco de mi personalidad y algunos gustos*]</h1>
+
+      <p></p>
+      <h1>[*Insertar Hobbies*]</h1>
+
+
+      <p></p>
+      <p>Desde pequeño siempre me ha gustado jugar videojuegos.</p>
+      <p>Uno de mis grandes sueños y objetivos es crear videojuegos que tengo en mi mente.</p>
+      <p>Por este sueño estoy estudiando Computer Science</p>
+      <h1>Actualmente estoy trabajando en el proyecto [*Insertar nombre y luego algunas imagenes*]</h1>
+
+      <p></p>
+      <p>Pero no solo me gustan los videojuegos.</p>
+      <p>También escogí la carrera porque me encanta la computación y programación.</p>
+      <p>Cuando tengo la oportunidad y el tiempo realizo varios ejercicios y desafios en páginas como:</p>
+      [*Insetar plataformas y enlace para mis perfiles*]
+
+      <p></p>
+      <p>Me encanta poder aprender varios temas de computer science, soy abierto a cualquier tipo de</p>
+      <p>conocimiento que pueda adquirir.</p>
+
+      <p></p>
+      <p>Habilidades</p>
+
+      <p></p>
+      <p>Lenguajes de Programación</p>
+
+      <p></p>
+      <p>Otros</p>
+
+      <p></p>
+      <h1>Carreras académicas</h1>
+
+      <h2>Colegio Adventista JUBA - [2016 - 2019]</h2>
+
+      <h2>Universidad del Valle de Guatemala - [2020 - 2024]</h2>
+
+      <p></p>
+      <h1>Carreras académicas</h1>
+
     </div>
   )
 }
