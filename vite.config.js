291f91414ea137b13084b5f43962066e3a3e2ad4
CRLB-Sketch <cristianlaynezbachez@gmail.com>
2023-04-24 10:39:57 -0400

    deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { imagetools } from 'vite-imagetools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), imagetools()],
+  base: "/vite-deploy",
   module: {
     rules: [{ test: /\.(png|jpg|gif|avif|jfif)$/, loader: 'url-loader' }],
   },
